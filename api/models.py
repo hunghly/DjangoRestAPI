a931a29d920b843ea472846bd3dd31fd1e11b51d
hunghly <hunghly@gmail.com>
2020-08-24 15:37:25 -0500

    added models

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Movie(models.Model):
@@ -7,5 +8,11 @@ class Movie(models.Model):
     description = models.TextField(max_length=360)
 
 class Rating(models.Model):
-    movie = models.ForeignKey(Movie)
-    user = models.ForeignKey(User)
+    # if movie is removed, then remove the rating (cascading)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    # if we already have a rating for a movie for that user, it will reject the entry because of this meta class
+    class Meta:
+        unique_together = (('user', 'movie'))
+        index_together = (('user', 'movie'))
